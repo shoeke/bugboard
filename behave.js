4090d09a65014043b1cb4e33c689fce5f566bcf0
sabrina <hoekehouse@gmail.com>
2020-02-14 19:21:09 -0800

    Paper finalized and sound added

--- behave.js
+++ behave.js
@@ -1,27 +1,20 @@
 $(document).ready(function(){
-/*
-      $("#button-slide").click(function(){
-        if ($("#shades").is(":visible")) {
-            $("#shades").hide();
-            $("#button-slide").css("transform","translate(73px,0)")
-        } else if ($("#shades").is(":hidden")) {
-            $("#shades").show();
-            $("#button-slide").css("transform","translate(0,0)");
-        }
-      });*/
 
       $("#button-slide").click(function(){
         if ($("#shades").css('opacity') == 1) {
             $("#shades").css("opacity", 0);
             $("#button-slide").css("transform","translate(73px,0)");
+            $('audio#click')[0].play();
         } else if ($("#shades").css('opacity') == 0) {
             $("#shades").css("opacity", 1);
             $("#button-slide").css("transform","translate(0,0)");
+            $('audio#click')[0].play();
         }
       });
 
       $("#box1").mouseenter(function() {
         $(".modal1").show();
+        $('audio#confirm')[0].play();
       });
       $("#box1").mouseleave(function() {
         $(".modal1").hide();
@@ -29,6 +22,7 @@ $(document).ready(function(){
 
       $("#box2").mouseenter(function() {
         $(".modal2").show();
+        $('audio#confirm')[0].play();
       });
       $("#box2").mouseleave(function() {
         $(".modal2").hide();
@@ -36,6 +30,7 @@ $(document).ready(function(){
 
       $("#box3").mouseenter(function() {
         $(".modal3").show();
+        $('audio#confirm')[0].play();
       });
       $("#box3").mouseleave(function() {
         $(".modal3").hide();
@@ -43,6 +38,7 @@ $(document).ready(function(){
 
       $("#box4").mouseenter(function() {
         $(".modal4").show();
+        $('audio#confirm')[0].play();
       });
       $("#box4").mouseleave(function() {
         $(".modal4").hide();
@@ -50,6 +46,7 @@ $(document).ready(function(){
 
       $("#box5").mouseenter(function() {
         $(".modal5").show();
+        $('audio#confirm')[0].play();
       });
       $("#box5").mouseleave(function() {
         $(".modal5").hide();
@@ -57,6 +54,7 @@ $(document).ready(function(){
 
       $("#box6").mouseenter(function() {
         $(".modal6").show();
+        $('audio#confirm')[0].play();
       });
       $("#box6").mouseleave(function() {
         $(".modal6").hide();
@@ -64,6 +62,7 @@ $(document).ready(function(){
 
       $("#box7").mouseenter(function() {
         $(".modal7").show();
+        $('audio#confirm')[0].play();
       });
       $("#box7").mouseleave(function() {
         $(".modal7").hide();
@@ -71,6 +70,7 @@ $(document).ready(function(){
 
       $("#box8").mouseenter(function() {
         $(".modal8").show();
+        $('audio#confirm')[0].play();
       });
       $("#box8").mouseleave(function() {
         $(".modal8").hide();
@@ -78,6 +78,7 @@ $(document).ready(function(){
 
       $("#box9").mouseenter(function() {
         $(".modal9").show();
+        $('audio#confirm')[0].play();
       });
       $("#box9").mouseleave(function() {
         $(".modal9").hide();
@@ -85,6 +86,7 @@ $(document).ready(function(){
 
       $("#box10").mouseenter(function() {
         $(".modal10").show();
+        $('audio#confirm')[0].play();
       });
       $("#box10").mouseleave(function() {
         $(".modal10").hide();
